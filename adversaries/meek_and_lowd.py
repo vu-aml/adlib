29eb9d99db1f4cad42adcf8ab27224501ea3cfc0
Ethan Raymond <ethan.raymond14@gmail.com>
2016-10-27 01:29:35 -0500

    meek and lowd edits

--- adversaries/meek_and_lowd.py
+++ adversaries/meek_and_lowd.py
@@ -68,9 +68,9 @@ class ConvexClassifierSearch:
   # I'm not sure what value epsilon is supposed to be (or cost_min)
   def convex_set_search(self, cost_vector, epsilon, cost_min, instance):
     x_a = instance.get_feature_vector()
-    x_minus = self.negative_instance.get_feature_vector()
+    x_minus = self.adversary.negative_instance.get_feature_vector()
     dimension = len(x_a)
-    cost_max =  feature_difference(x_minux, x_a);
+    cost_max =  self.adversary.feature_difference(x_minux, x_a);
     search_set = set()
     for i in range(dimension):
       # TODO: x_a[i] is wrong, its actually [0,0,0,..,1,..,0] where 1 is the ith feature
@@ -89,7 +89,7 @@ class ConvexClassifierSearch:
       element = search_set.pop()
       new_search_set.add(element)
       prediction_weight = cost_max * pow(2, pow(-2,t))
-      query_result = self.learn_model.predict(Instance(0, x_a + prediction_weight * element))
+      query_result = self.adversary.learn_model.predict(Instance(0, x_a + prediction_weight * element))
       if prediction_weight == -1:
         search_set.add(element)
         new_search_set = set()
@@ -100,14 +100,13 @@ class ConvexClassifierSearch:
 
 class MultiLineSearch(ConvexClassifierSearch):
 
-  def __init__(self, epsilon, cost_min, cost_vector):
-    self.epsilon = epsilon
-    self.cost_min = cost_min
-    self.cost_vector = cost_vector
-
   def execute(self, instance):
-    search_set, cost_min, cost_max, epsilon, instance =
-     self.convex_set_search(self.cost_vector, self.epsilon, self.cost_min, instance)
+    search_set, cost_min, cost_max, epsilon, instance = self.convex_set_search(
+      self.adversary.cost_vector,
+      self.adversary.epsilon,
+      self.adversary.cost_min,
+      instance
+    )
     search_set, cost_min, cost_max = self.spiral_search(search_set, cost_max)
     return self.multi_line_search(search_set, cost_min, cost_max, epsilon, instance)
 
@@ -115,7 +114,7 @@ class MultiLineSearch(ConvexClassifierSearch):
   # C_plus = initial lower bound on cost
   # C_minus = initial upper bound on cost
   def multi_line_search(self, W, cost_plus, cost_minus, epsilon, instance):
-		x_minus = self.negative_instance.get_feature_vector()
+    x_minus = self.adversary.negative_instance.get_feature_vector()
     x_star = deepcopy(x_minus)
     # x_a subset of positive instances
     x_a = instance.get_feature_vector()
@@ -124,12 +123,12 @@ class MultiLineSearch(ConvexClassifierSearch):
       is_negative_vertex_found = False
       for e in W:
         # looks like we're assuming that y and e have the same dimensions
-        if self.learn_model.predict(Instance(0, y + cost_t * e)) == -1:
+        if self.adversary.learn_model.predict(Instance(0, y + cost_t * e)) == -1:
           x_star = y + cost_t * e
           is_negative_vertex_found = True
           # Prune all costs that result in positive prediction
           for i in W:
-            if self.learn_model.predict(Instance(0, y + cost_t * i)) == 1:
+            if self.adversary.learn_model.predict(Instance(0, y + cost_t * i)) == 1:
               # deleting from a list when you iterate through it may cause bugs
               W.delete(i)
           break
@@ -143,20 +142,16 @@ class MultiLineSearch(ConvexClassifierSearch):
 
 class KStepMultiLineSearch(MultiLineSearch):
 
-  def __init__(self, epsilon, cost_min, cost_vector, k):
-    self.k = k
-    super.__init__(epsilon, cost_min, cost_vector)
-
   def multi_line_search(self, search_set, cost_min, cost_max, epsilon, instance):
     x_a = instance.get_feature_vector()
-    x_star = self.negative_instance.get_feature_vector()
+    x_star = self.adversary.negative_instance.get_feature_vector()
     while cost_max / cost_min > 1 + epsilon:
       e = search_set.pop() # pop removes an element which might not be what we want to do
       temp_min_cost = cost_min
       temp_max_cost = cost_max
-      for i in range(self.k):
+      for i in range(self.adversary.k):
         temp_cost = sqrt(temp_min_cost * temp_max_cost)
-        query_result = self.learn_model.predict(Instance(0, x_a + temp_cost * e))
+        query_result = self.adversary.learn_model.predict(Instance(0, x_a + temp_cost * e))
         if query_result == 1:
           temp_min_cost = temp_cost
         else:
@@ -164,7 +159,7 @@ class KStepMultiLineSearch(MultiLineSearch):
           x_star = x_a + temp_cost * e
       positive_directions = set()
       for i in search_set: # we know i != e because e was removed
-        query_result = self.learn_model.predict(Instance(0, x_a + temp_min_cost * i))
+        query_result = self.adversary.learn_model.predict(Instance(0, x_a + temp_min_cost * i))
         if query_result == -1:
           x_star = x_a + temp_min_cost * i
           # Proon positive directions
@@ -184,69 +179,87 @@ class Adversary(AdversaryStrategy):
   MULTI_LINE_SEARCH = 'multi_line_search'
   K_STEP_MULTI_LINE_SEARCH = 'k_step_multi_line_search'
 
-	def __init__(self):
-		self.learn_model = None        # type: InitialPredictor
-		self.positive_instance = None  # type: Instance
-		self.negative_instance = None  # type: Instance
+  def __init__(self):
+    self.learn_model = None        # type: InitialPredictor
+    self.positive_instance = None  # type: Instance
+    self.negative_instance = None  # type: Instance
+    self.adversary_costs = None  # type: Array 
+    self.epsilon = None  # type: Double?
+    self.cost_min = None  # type: Double?
+    self.k = None  # type: integer
     self.attack_model_type = Adversary.FIND_BOOLEAN_IMAC
 
-	def change_instances(self, instances: List[Instance]) -> List[Instance]:
-		'''Change adversarial instances by finding boolean IMAC.
+  def change_instances(self, instances: List[Instance]) -> List[Instance]:
+    '''Change adversarial instances by finding boolean IMAC.
 
-		used to transform all test instances, assuming that each instance classified as
-		adversarial is the 'ideal' instance. To find a single IMAC based on a user specified
-		adversarial 'ideal' instance, override test data to pass in single (or multiple)
-		adversarially preferred instances.
+    used to transform all test instances, assuming that each instance classified as
+    adversarial is the 'ideal' instance. To find a single IMAC based on a user specified
+    adversarial 'ideal' instance, override test data to pass in single (or multiple)
+    adversarially preferred instances.
 
-		'ideal' instance refers to the test vector that makes the best argument for the
-		adversaries intention. E.g. spam filtering, email that the adversary classifies as being
-		most useful to their efforts
-		'''
-		transformed_instances = []
+    'ideal' instance refers to the test vector that makes the best argument for the
+    adversaries intention. E.g. spam filtering, email that the adversary classifies as being
+    most useful to their efforts
+    '''
+    transformed_instances = []
 
-		for instance in instances:
-			transformed_instance = deepcopy(instance)
-			if instance.get_label() == 1:
-				transformed_instances.append(self.attack_model.execute(instance))
-			else:
-				transformed_instances.append(transformed_instance)
-		return transformed_instances
+    for instance in instances:
+      transformed_instance = deepcopy(instance)
+      if instance.get_label() == 1:
+        transformed_instances.append(self.attack_model.execute(instance))
+      else:
+        transformed_instances.append(transformed_instance)
+    return transformed_instances
 
-	def get_available_params(self):
-		return None
+  def get_available_params(self):
+    return {
+      'attack_model_type': self.attack_model_type,
+      'adversary_costs': self.adversary_costs,
+      'epsilon': self.epsilon,
+      'cost_min': self.cost_min,
+      'k': self.k,
+    }
 
-	def set_params(self, params: Dict):
-		if params['attack_model'] is not None:
-			self.attack_model_type = params['attack_model']
-		
+  def set_params(self, params: Dict):
+    if params['attack_model_type'] is not None:
+      self.attack_model_type = params['attack_model_type']
+    
     if params['adversary_costs'] is not None:
-			self.adversary_costs = params['adversary_costs']
+      self.adversary_costs = params['adversary_costs']
 
     if params['epsilon'] is not None:
-			self.epsilon = params['epsilon']
-		
+      self.epsilon = params['epsilon']
+    
     if params['cost_min'] is not None:
-			self.cost_min = params['cost_min']
-		
+      self.cost_min = params['cost_min']
+    
     if params['k'] is not None:
-			self.k = params['k']
-		
+      self.k = params['k']
+    
     return None
 
-	def set_adversarial_params(self, learner, train_instances):
-		self.learn_model = learner
-		instances = train_instances # type: List[Instance]
-		self.positive_instance = next((x for x in instances if x.get_label() == 1), None)
-		self.negative_instance = next((x for x in instances if x.get_label() == -1), None)
+  def feature_difference(self, y: FeatureVector, xa: FeatureVector) -> List:
+    y_array = y.get_csr_matrix()
+    xa_array = xa.get_csr_matrix()
+
+    C_y = (y_array - xa_array).indices
+
+    return C_y
+
+  def set_adversarial_params(self, learner, train_instances):
+    self.learn_model = learner
+    instances = train_instances # type: List[Instance]
+    self.positive_instance = next((x for x in instances if x.get_label() == 1), None)
+    self.negative_instance = next((x for x in instances if x.get_label() == -1), None)
 
     if self.attack_model_type is Adversary.FIND_BOOLEAN_IMAC:
-      self.attack_model = FindBooleanIMAC()
+      self.attack_model = FindBooleanIMAC(self)
 
     elif self.attack_model_type is Adversary.MULTI_LINE_SEARCH:
-      self.attack_model = MultiLineSearch(self.epsilon, self.cost_min, self.cost_vector)
+      self.attack_model = MultiLineSearch(self)
 
     elif self.attack_model_type is Adversary.K_STEP_MULTI_LINE_SEARCH:
-      self.attack_model = KStepMultiLineSearch(self.epsilon, self.cost_min, self.cost_vector, k)
+      self.attack_model = KStepMultiLineSearch(self)
 
   # This is a uniform adversarial cost function, should we add a weight parameter?
   # TODO: figure out if I need to implement these

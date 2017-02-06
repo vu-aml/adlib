65d3153e9a2900eab359ac105532e881d8891f1c
Max deGroot <alexander.m.degroot@vanderbilt.edu>
2017-02-06 15:19:47 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,12 +1,19 @@
-from distutils.core import setup
+from setuptools import find_packages, setup
 
 setup(
-    name='pyamll_1_0',
-    version='',
-    packages=['learners', 'data_reader', 'adversaries'],
-    url='',
+    name='pyamll',
+    version='0.0.1',
+    description="Game-theoretic adversarial machine learning library providing a set of learner and adversary modules.",
+    url='https://github.com/yvorobey/aml',
     license='',
-    author='Alexandra',
-    author_email='',
-    description=''
+    author='Alexandra, Alexander deGroot, Ethan Raymond, Jiazhi Zhang, Bumsu Jung, Yevgeniy Vorobeychik',
+    author_email='yevgeniy.vorobeychik@vanderbilt.edu',
+    platforms=['any'],
+    packages=find_packages(),
+    install_requires=[
+        'numpy>=1<2',
+        'scipy>=0.15',
+        'sklearn>=0.18',
+        'matplotlib>=2.0.0',
+    ]
 )

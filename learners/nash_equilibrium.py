1121e4d259f7619f8030476029e90c26b8f109ca
Ethan Raymond <ethan.raymond14@gmail.com>
2017-03-08 19:16:19 -0500

    convert nash eq learner interface

--- learners/nash_equilibrium.py
+++ learners/nash_equilibrium.py
@@ -1,4 +1,4 @@
-from learners.learner import InitialPredictor, ImprovedPredictor
+from learners.learner import RobustLearner
 from typing import List
 from types import FunctionType
 import numpy as np
@@ -19,30 +19,54 @@ Concept:
 
 """
 
-class Learner(InitialPredictor):
+class NashEquilibrium(RobustLearner):
+    def __init__(self):
+        """New generic initial learner with no specified learning model.
 
-	def __init__(self):
-		InitialPredictor.__init__(self)
-
-	def loss_function(self, z: float, y: int):
-		return np.log(1 + np.exp(-1 * y * z))
-
-	def get_loss_function(self) -> FunctionType:
-		return getattr(self, 'loss_function')
-
-
-class ImprovedLearner(ImprovedPredictor):
-
-	def __init__(self):
-		ImprovedPredictor.__init__(self)
+        """
+        RobustLearner.__init__(self)
 		self.weight_vector = None
 
-	def predict(self, instances: List[Instance]):
-		predictions = []
+    def train(self):
+        """Train on the set of training instances.
+
+        """
+        raise NotImplementedError
+
+    def predict(self, instances):
+        """Predict classification labels for the set of instances.
+
+        Args:
+            instances (List[Instance]) or (Instance): training or test instances.
+
+        Returns:
+            label classifications (List(int))
+
+        """
+        if not self.weight_vector:
+            raise ValueError('Weight vector cannot be None')
+
+        predictions = []
 		for instance in instances:
 			features = instance.get_feature_vector().get_csr_matrix().toarray()[0]
 			predictions.append(np.sign(predict_instance(features, self.weight_vector)))
 		return predictions
 
-	def set_adversarial_params(self, learner: Learner, adversary: Adversary):
-		self.weight_vector = adversary.get_learner_params()
+    def set_params(self, params: Dict):
+        """Set params for the initial learner.
+
+        Defines default behavior, setting only BaseModel params
+
+        Args:
+            params (Dict): set of available params with updated values.
+
+        """
+        if params['adversary'] is not None:
+		    self.weight_vector = params['adversary'].get_learner_params()
+
+    # not sure we need these	
+    def loss_function(self, z: float, y: int):
+		return np.log(1 + np.exp(-1 * y * z))
+
+	def get_loss_function(self) -> FunctionType:
+		return getattr(self, 'loss_function')

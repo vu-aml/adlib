0b139c0de7962b119c166b8875435922cd1b39fa
Zhang Jiazhi <JasonZ@Zhangs-MacBook-Pro.local>
2017-03-11 23:18:17 -0600

    feature_deletion learner fixed with CVXPY

--- learners/feature_deletion.py
+++ learners/feature_deletion.py
@@ -1,9 +1,9 @@
 from learners.learner import RobustLearner
-
 from data_reader.input import Instance
+from data_reader.operations import sparsify
 from typing import List, Dict
 import numpy as np
-from cvxopt import matrix, solvers
+from cvxpy import *
 
 
 
@@ -14,21 +14,23 @@ class FeatureDeletion(RobustLearner):
     def __init__(self, params=None, training_instances=None):
         RobustLearner.__init__(self)
         self.weight_vector = None
-        self.num_features = None  # type: int
+        self.num_features = 0  # type: int
         self.hinge_loss_multiplier = 0.5  # type: float
         self.max_feature_deletion = 30  # type: int
 
-        self.weight_vector = None  # type: np.array
+        self.weight_vector = None   # type: np.array
+                                    # of shape (1, self.num_features)
         self.bias = 0  # type: int
 
         self.set_params(params)
         self.set_training_instances(training_instances)
+        print(self.num_features)
 
     def set_params(self, params: Dict):
-        if params['hinge_loss_multiplier'] is not None:
+        if 'hinge_loss_multiplier' in params:
             self.hinge_loss_multiplier = params['hinge_loss_multiplier']
 
-        if params['max_feature_deletion'] is not None:
+        if 'max_feature_deletion' in params:
             self.max_feature_deletion = params['max_feature_deletion']
 
     def get_available_params(self) -> Dict:
@@ -46,70 +48,57 @@ class FeatureDeletion(RobustLearner):
         """
         num_instances = len(self.training_instances)
 
-        X_neg = [ins.feature_vector for ins in self.training_instances
-                 if ins.get_label() == RobustLearner.negative_classification]
-        X_pos = [ins.feature_vector for ins in self.training_instances
-                 if ins.get_label() == RobustLearner.positive_classification]
+        y_list, X_list = sparsify(self.training_instances)
+        y, X = np.array(y_list), X_list.toarray()
 
-        y_list = y_list = [ins.label for ins in self.training_instances]
-
+        C = self.hinge_loss_multiplier
+        K = self.max_feature_deletion
 
 
-        X_list = [ins.feature_vector for ins in self.training_instances]
-        y_list = [ins.label for ins in self.training_instances]
-
-        xlen = self.num_features + num_instances * 2 + num_instances * self.num_features
-        wi_start = 0
-        ti_start = wi_start + self.num_features
-        zi_start = ti_start + self.num_features
-        v_start = zi_start + self.num_features
-
-        P = np.zeros((xlen, xlen))
-        identity = np.multiply(np.identity(self.num_features), 1 / 2)
-        P[:self.num_features, :self.num_features] = identity
-
-        weight_terms = [sum(y_list[i] * X_list[i][j] for i in range(num_instances)) * self.weight_vector[j]
-                        for j in range(self.num_features)]
-        q_list = weight_terms + [1] * num_instances + [0] * (xlen - num_instances - self.num_features)
-        q = np.array(q_list)
-        np.multiply(q, self.hinge_loss_multiplier)
-
-        G_list = np.array([[0] * xlen])
+        i_ones, i_zeroes, j_ones = np.ones(num_instances), np.zeros(num_instances), np.ones(self.num_features)
+        w = Variable(self.num_features)  # weight vector
+        b = Variable()  # bias term
+        t = Variable(num_instances)
+        z = Variable(num_instances)
+        v = {}
+        loss = sum_entries(pos(1 - mul_elemwise(y, X * w - b) + t))  # loss func
         for i in range(num_instances):
-            lst = [0] * xlen
-            lst[ti_start + i] = 1
-            lst[zi_start + i] = -self.max_feature_deletion
-            lst[v_start + i * self.num_features:v_start + (i + 1) * self.num_features] = [1] * self.num_features
-            np.append(G_list, [np.array(lst)])
-        for i in range(v_start, xlen):
-            lst = [0] * xlen
-            lst[i] = 1
-            np.append(G_list, [np.array(lst)])
-        for i in range(num_instances):
-            for j in range(self.num_features):
-                lst = [0] * xlen
-                lst[wi_start + j] = -y_list[i] * X_list[i][j]
-                lst[v_start + i * self.num_features + j] = 1
-                lst[zi_start + i] = 1
-                np.append(G_list, [np.array(lst)])
-        G = G_list[1:]
+            v[i] = Variable(self.num_features)
+        yX = y.dot(X)
 
-        h = np.array([0] * len(G))
+        constraints = [t >= K * z + np.dot(X, j_ones)]
 
-        sol = solvers.qp(matrix(P), matrix(q), matrix(G), matrix(h))
+        # add constraints vi >= 0
+        constraints.extend([v[i] >= 0 for i in range(num_instances)])
 
-        self.weight_vector = sol[:self.num_features]
+        # add constraints zi + vi >= y.dot(X) * w
+        constraints.extend([v[i][j] + z[i] >= yX[j]*w[j] for j in range(self.num_features)
+                                     for i in range(num_instances)])
+
+        obj = Minimize(0.5*(norm(w)) + C * loss)
+
+        prob = Problem(obj, constraints)
+        prob.solve()
+
+        self.weight_vector = [np.array(w.value).T][0]   # w.value is of shape [1, self.num_features]
+        self.bias = b.value
+
 
     def predict(self, instances: List[Instance]):
         predictions = []
         for instance in instances:
-            features = instance.get_feature_vector().get_csr_matrix().toarray()[0]
+            features = instance.get_feature_vector().get_csr_matrix().toarray()
             predictions.append(np.sign(self.predict_instance(features)))
         return predictions
 
     def predict_instance(self, features):
-        return np.dot(features, self.weight_vector)
+        '''
+        predict class for a single instance and return a real value
+        :param features: np.array of shape (1, self.num_features), i.e. [[1, 2, ...]]
+        :return: float
+        '''
+        return self.weight_vector.dot(features.T)[0][0] + self.bias
 
     def predict_proba(self, instances: List[Instance]):
         return [self.predict_instance(
-            ins.get_feature_vector().get_csr_matrix().toarray()[0]) for ins in instances]
+            ins.get_feature_vector().get_csr_matrix().toarray()) for ins in instances]

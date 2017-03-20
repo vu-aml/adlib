0838c7c161498e17b7b0f853a52f5e993d21e013
Ethan Raymond <ethan.raymond14@gmail.com>
2017-03-20 15:22:15 -0500

    remove merge conflict

--- learners/__init__.py
+++ learners/__init__.py
@@ -1,16 +1,8 @@
-<<<<<<< HEAD
-from learners.learner import RobustLearner
-from learners.feature_deletion import FeatureDeletion
-from learners.retraining import Retraining
-from learners.simple_learner import SimpleLearner
-from learners.stackelberg import StackelbergLearner
-from learners.nash_equilibrium import NashEquilibrium
-=======
 from .learner import RobustLearner
 from .feature_deletion import FeatureDeletion
 from .retraining import Retraining
 from .simple_learner import SimpleLearner
 from .stackelberg import StackelbergLearner
+from learners.nash_equilibrium import NashEquilibrium
 from .svm_restrained import SVMRestrained
 from .svm_freerange import SVMFreeRange
->>>>>>> 9cda1e1f04790317c30b6bfd84916c6f3164128f

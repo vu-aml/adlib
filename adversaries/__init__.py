6c26b769ce557000bd0e6b8808c0a5493361a449
Ethan Raymond <ethan.raymond14@gmail.com>
2017-04-03 12:21:43 -0500

    remove gradient descent from adv init

--- adversaries/__init__.py
+++ adversaries/__init__.py
@@ -2,5 +2,4 @@ from .adversary import Adversary
 from .cost_sensitive import CostSensitive
 from .free_range import FreeRange
 from .good_word import GoodWord
-from .gradient_descent import GradientDescent
 from .simple_optimize import SimpleOptimize

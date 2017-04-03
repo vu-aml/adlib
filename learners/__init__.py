497309435f6c9ef56339f30aa2d72a106c81e360
Ethan Raymond <ethan.raymond14@gmail.com>
2017-04-02 23:16:56 -0500

    update __init__ files and remove meek and lowd

--- learners/__init__.py
+++ learners/__init__.py
@@ -2,7 +2,5 @@ from .learner import RobustLearner
 from .feature_deletion import FeatureDeletion
 from .retraining import Retraining
 from .simple_learner import SimpleLearner
-from .stackelberg import StackelbergLearner
-from learners.nash_equilibrium import NashEquilibrium
 from .svm_restrained import SVMRestrained
 from .svm_freerange import SVMFreeRange

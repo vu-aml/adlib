a8acaaee6a7c0015a5208632506930d8a62b58ba
Jiazhi Zhang <jiazhi.zhang@vanderbilt.edu>
2018-02-06 10:53:30 -0600

    server run script

--- Experiment/MultiProcessEnronFD.py
+++ Experiment/MultiProcessEnronFD.py
@@ -1,3 +1,5 @@
+import sys
+sys.path.append("/home/zhanj16/abc/adlib")
 from sklearn import svm
 from learners import SimpleLearner
 import learners as learner
@@ -5,11 +7,10 @@ from data_reader.dataset import EmailDataset
 from data_reader.operations import load_dataset
 from adversaries.feature_deletion import AdversaryFeatureDeletion
 from sklearn import metrics
-from multiprocessing import Process
+import multiprocessing
+import numpy as np
 
-#Passing
-
-def summary(y_pred, y_true):
+def single_run_dict(y_pred, y_true):
     if len(y_pred) != len(y_true):
         raise ValueError("lengths of two label lists do not match")
     tp, fp, tn, fn = 0,0,0,0
@@ -25,11 +26,10 @@ def summary(y_pred, y_true):
     acc = metrics.accuracy_score(y_true, y_pred)
     prec = metrics.precision_score(y_true, y_pred)
     rec = metrics.recall_score(y_true, y_pred)
-    s = "accuracy: {0} \n precision: {1} \n recall: {2}\n".format(acc, prec, rec)
-    s += "TP {0}, FP {1}, TN {2}, FN {3}".format(tp,fp,tn,fn)
-    return s
+    result = dict(accuracy=acc, precision=prec, recall=rec)
+    return result
 
-def run():
+def run(del_cnt=10):
     dataset = EmailDataset(path='../data_reader/data/enron/enron1/index_dir',binary= False,raw=True)
     training_, testing_ = dataset.split(0.6)
     training_data = load_dataset(training_)
@@ -42,23 +42,35 @@ def run():
     learner1.train()
 
     predictions = learner1.predict(testing_data)
-    print("======== initial prediction =========")
-    print(summary(predictions, test_true_label))
+    # print("======== initial prediction =========")
+    # print(summary(predictions, test_true_label))
+    result1 = single_run_dict(predictions, test_true_label)
 
 
     #test Restrained_attack
-    attacker = AdversaryFeatureDeletion()
+    attacker = AdversaryFeatureDeletion(num_deletion=del_cnt)
     attacker.set_adversarial_params(learner1, testing_data)
     new_testing_data = attacker.attack(testing_data)
 
     predictions2 = learner1.predict(new_testing_data)
-    print("========= post-attack prediction =========")
-    print(summary(predictions2, test_true_label))
+    result2 = single_run_dict(predictions2, test_true_label)
+
+    s = "run with variable param value: {0}\n".format(del_cnt)
+    s += str(result1) + "\n"
+    s += str(result2) + "\n"
+    return s
+
+
+def generate_interval(start, end, process_count, dtype=None):
+    return (np.linspace(start, end, process_count, dtype=dtype)).tolist()
 
 
 if __name__ == '__main__':
-    jobs = []
-    for i in range(5):
-        p = Process(target=run)
-        jobs.append(p)
-        p.start()
+    pool = multiprocessing.Pool(processes=4)
+    varyingParam = generate_interval(1,100, 4, dtype=int)
+
+    result_list = pool.map(run, varyingParam)
+    with open('FD_result.txt', 'a') as the_file:
+        for r in result_list:
+            the_file.write(r)
+            print(r)

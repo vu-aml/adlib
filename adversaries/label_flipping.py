5cf032eb16475b6a397ed367e48120e4ed15fea0
Matthew Sedam <sedammatthew@gmail.com>
2018-05-07 16:56:11 -0500

    Added tests for label flipping and refactor

--- adversaries/label_flipping.py
+++ adversaries/label_flipping.py
@@ -8,61 +8,51 @@ from typing import List, Dict
 
 class LabelFlipping(Adversary):
 
-    def __init__(self, learner, train_instances: List[Instance],
-                 cost: List[float], total_cost: float, gamma=0.05):
-
-        if len(cost) != len(train_instances):
-            raise ValueError('cost must have same length as train_instances')
+    def __init__(self, learner, cost: List[float], total_cost: float,
+                 gamma=0.05):
 
         Adversary.__init__(self)
         self.learner = learner
-        self.orig_train_instances = train_instances
-        self.train_instances = train_instances
-        temp = copy.deepcopy(train_instances)
-        for i in temp:
-            i.label = -i.label
-        self.train_instances += temp
-        self.cost = np.append(np.zeros(len(train_instances)), np.array(cost))
+        self.cost = cost
         self.total_cost = total_cost
         self.gamma = gamma
 
     def attack(self, instances) -> List[Instance]:
-        pred_labels = self.learner.predict(self.orig_train_instances)
+        n = len(instances) * 2
+        pred_labels = self.learner.predict(instances)
         orig_loss = []
         for i in range(len(pred_labels)):
-            orig_loss.append((pred_labels[i] -
-                              self.orig_train_instances[i].get_label()) ** 2)
+            orig_loss.append((pred_labels[i] - instances[i].get_label()) ** 2)
         orig_loss = np.array(orig_loss + orig_loss)
 
         # Setup CVX
 
-        q = cvx.Variable(len(self.train_instances))
-        w = cvx.Variable(len(self.train_instances))
-        epsilon = cvx.Variable(len(self.train_instances))
+        q = cvx.Variable(n)
+        w = cvx.Variable(n)
+        epsilon = cvx.Variable(n)
 
         func = cvx.sum_entries(cvx.mul_elemwise(q, epsilon - orig_loss))
-        func += len(self.train_instances) * self.gamma * (cvx.norm(w) ** 2)
+        func += n * self.gamma * (cvx.norm(w) ** 2)
 
         feature_vectors = []
-        for inst in self.train_instances:
-            feature_vectors.append(inst.get_feature_vector())
-        feature_vectors = np.array(feature_vectors)
-
         labels = []
-        for inst in self.train_instances:
+        for inst in instances:
+            feature_vectors.append(inst.get_feature_vector())
             labels.append(inst.get_label())
-        labels = np.array(labels)
+        feature_vectors = np.array(feature_vectors + feature_vectors)
+        labels = np.array(labels + labels)
+
+        cost = np.array((self.cost + self.cost)[n])
 
         constraints = [
-            cvx.sum_entries(cvx.mul_elemwise(self.cost, q)) <= self.total_cost]
-        for i in range(len(self.train_instances)):
+            cvx.sum_entries(cvx.mul_elemwise(cost, q)) <= self.total_cost]
+        for i in range(n):
             constraints.append(1 - labels[i] * cvx.sum_entries(
                 cvx.mul_elemwise(w, feature_vectors[i])) <= epsilon[i])
             constraints.append(epsilon[i] >= 0)
             constraints.append(q[i] == 0 or q[i] == 1)
-            if i < len(self.orig_train_instances):
-                constraints.append(
-                    q[i] + q[i + len(self.orig_train_instances)] == 1)
+            if i < (n / 2):
+                constraints.append(q[i] + q[i + (n / 2)] == 1)
 
         prob = cvx.Problem(cvx.Minimize(func), constraints)
         result = prob.solve()
---
+++ tests/adversaries/label_flipping_test.py
@@ -0,0 +1,28 @@
+from sklearn import svm
+from learners import SimpleLearner
+import numpy as np
+from data_reader.dataset import EmailDataset
+from data_reader.operations import load_dataset
+from adversaries.label_flipping import LabelFlipping
+
+
+def test_label_flipping():
+    # Data processing unit
+    # The path is an index of 400 testing samples(raw email data).
+    dataset = EmailDataset(path='../../data_reader/data/raw/trec05p-1/test-400',
+                           binary=True, raw=True)
+    training_, testing_ = dataset.split({'train': 60, 'test': 40})
+    training_data = load_dataset(training_)
+    testing_data = load_dataset(testing_)
+
+    # Setting the default learner
+    # Test simple learner svm
+    learning_model = svm.SVC(probability=True, kernel='linear')
+    learner = SimpleLearner(learning_model, training_data)
+    learner.train()
+
+    # Execute the attack
+    cost = list(np.full(len(training_data), 1))  # cost of flip is constant 1
+    total_cost = 0.1 * len(training_data)  # flip at most 10% of labels
+    attacker = LabelFlipping(learner, cost, total_cost)
+    result = attacker.attack(training_data)

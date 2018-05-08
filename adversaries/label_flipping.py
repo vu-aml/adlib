0400d265c7c1cbc47b017fe27d67efe47944dac6
Matthew Sedam <sedammatthew@gmail.com>
2018-05-08 17:30:11 -0500

    Fix CVX problems (2/2) and implement half of the alternating approximation loop

--- adversaries/label_flipping.py
+++ adversaries/label_flipping.py
@@ -37,60 +37,117 @@ class FeatureVectorWrapper(np.ndarray):
 class LabelFlipping(Adversary):
 
     def __init__(self, learner, cost: List[float], total_cost: float,
-                 gamma=0.05):
+                 gamma=0.05, num_iterations=10):
         Adversary.__init__(self)
         self.learner = learner
         self.cost = cost
         self.total_cost = total_cost
         self.gamma = gamma
+        self.num_iterations = num_iterations
 
     def attack(self, instances) -> List[Instance]:
-        if len(self.cost) != len(instances):
+        if len(instances) == 0 or len(self.cost) != len(instances):
             raise ValueError('Cost data does not match instances.')
 
-        n = len(instances) * 2
+        n = len(instances) * 2  # size of new (doubled) input
         pred_labels = self.learner.predict(instances)
         orig_loss = []
         for i in range(len(pred_labels)):
             orig_loss.append((pred_labels[i] - instances[i].get_label()) ** 2)
-        orig_loss = np.array(orig_loss + orig_loss)
+        orig_loss = np.array(orig_loss + orig_loss)  # eta in formula
 
-        # Setup CVX
-        q = cvx.Int(n)
-        w = cvx.Variable(instances[0].get_feature_count())
-        epsilon = cvx.Variable(n)
+        # Using alternating minimization. First fix q then minimize epsilon and
+        # w. Then, fix epsilon and w and minimize q. The first q will be
+        # generated randomly and follow the constraints that the total cost
+        # is less than total_cost.
+        #
+        # Formula: minimize <q, epsilon> + n * self.gamma * (||w||_2 ** 2) -
+        # <q, eta>, where <x,y> is the dot product of x and y. See book for
+        # constraints.
 
-        func = q.T * (epsilon - orig_loss) + n * self.gamma * (cvx.norm(w) ** 2)
-
-        feature_vectors = []
-        labels = []
-        for inst in instances:
-            feature_vectors.append(inst.get_feature_vector())
-            labels.append(inst.get_label())
-        feature_vectors = np.array(feature_vectors + feature_vectors)
-        labels = np.array(labels + labels)
+        ########################################################################
+        # Generate initial q by choosing self.total_cost / average_cost values
+        # from a uniform distribution and then satisfying constraints
 
         cost = np.concatenate([np.full(int(n / 2), 0), np.array(self.cost)])
+        average_cost = 0.0
+        for i in self.cost:
+            average_cost += i
+        average_cost /= len(self.cost)
+        num_to_pick = int(self.total_cost / average_cost)
 
-        constraints = [cost.T * q <= self.total_cost]
-        for i in range(n):
-            constraints.append(1 - labels[i] * w.T * feature_vectors[i]
-                               <= epsilon[i])
-            constraints.append(0 <= epsilon[i])
-            constraints.append(0 <= q[i])
-            constraints.append(q[i] <= 1)
-            if i < int(n / 2):
-                constraints.append(q[i] + q[i + int(n / 2)] == 1)
+        q = np.full(n, 0)
+        tmp = np.random.uniform(0, n, num_to_pick)
+        picked_indices = []
+        for i in tmp:
+            picked_indices.append(int(i))
 
-        prob = cvx.Problem(cvx.Minimize(func), constraints)
-        result = prob.solve()
-        print(result)  # ####################
+        running_cost = 0.0
+        for i in picked_indices:
+            if q[i] != 1:  # Only consider if we have not picked it yet
+                if running_cost + cost[i] <= self.total_cost:  # cost constraint
+                    q[i] = 1
+                    running_cost += cost[i]
 
-    def set_params(self, params: Dict):
-        raise NotImplementedError
+        for i in range(n // 2):  # Makes sure q[i] + q[i + n // 2] == 1 is True
+            if q[i] == 0:
+                if q[i + n // 2] == 0:
+                    q[i] = 1
+            else:
+                if q[i + n // 2] == 1:
+                    q[i + n // 2] = 0
 
-    def get_available_params(self):
-        raise NotImplementedError
+        ########################################################################
+        # Alternating minimization loop
 
-    def set_adversarial_params(self, learner, train_instances):
-        raise NotImplementedError
+        old_q, old_epsilon, old_w = q, None, None
+        flip = True
+        for i in range(self.num_iterations):
+            if flip:  # q is fixed, minimize over w and epsilon
+                # Setup variables
+                w = cvx.Variable(instances[0].get_feature_count())
+                epsilon = cvx.Variable(n)
+
+                # Calculate constants - can possibly ignore this as it only
+                # shifts the function - maybe
+                cnst = q.dot(orig_loss)
+
+                # Setup CVX problem
+                func = self.gamma * n * (cvx.pnorm(w, 2) ** 2) - cnst
+                for i in range(n):
+                    func += q[i] * epsilon[i]
+
+                feature_vectors = []
+                labels = []
+                for inst in instances:
+                    feature_vectors.append(
+                        FeatureVectorWrapper(inst.get_feature_vector()))
+                    labels.append(inst.get_label())
+                feature_vectors = np.array(feature_vectors + feature_vectors)
+                labels = np.array(labels + labels)
+
+                constraints = []
+                for i in range(n):
+                    constraints.append(1 - labels[i] * w.T * feature_vectors[i]
+                                       <= epsilon[i])
+                    constraints.append(0 <= epsilon[i])
+
+                prob = cvx.Problem(cvx.Minimize(func), constraints)
+                prob.solve()
+
+                old_epsilon = np.array(epsilon.value).flatten()
+                old_w = np.array(w.value).flatten()
+            # else:  # w and epsilon are fixed, minimize over q
+            #     q = cvx.Int(n)
+
+
+def set_params(self, params: Dict):
+    raise NotImplementedError
+
+
+def get_available_params(self):
+    raise NotImplementedError
+
+
+def set_adversarial_params(self, learner, train_instances):
+    raise NotImplementedError

ba7ec3724015d244b3c7a67bf1c78e677813b6d7
Matthew Sedam <sedammatthew@gmail.com>
2018-05-11 13:17:02 -0500

    Fixed setup.py

--- setup.py
+++ setup.py
@@ -2,11 +2,11 @@ from setuptools import find_packages, setup
 
 setup(
     name='adlib',
-    version='0.0.1',
+    version='0.1.0',
     description="Game-theoretic adversarial machine learning library providing a set of learner and adversary modules.",
     url='https://github.com/vu-aml/adlib',
-    license='',
-    author='Alexandra, Alexander deGroot, Ethan Raymond, Jiazhi Zhang, Bumsu Jung, Yevgeniy Vorobeychik',
+    license='MIT',
+    author='Alexandra, Alexander deGroot, Ethan Raymond, Jiazhi Zhang, Bumsu Jung, Yevgeniy Vorobeychik, Matthew Sedam',
     author_email='yevgeniy.vorobeychik@vanderbilt.edu',
     platforms=['any'],
     packages=find_packages(),

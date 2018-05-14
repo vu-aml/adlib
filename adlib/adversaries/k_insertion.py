ee349ad0d8ba817e38dab1660795803608c50389
Matthew Sedam <sedammatthew@gmail.com>
2018-05-14 11:35:09 -0500

    Added 2/4 kernels and derivatives

--- adlib/adversaries/k_insertion.py
+++ adlib/adversaries/k_insertion.py
@@ -5,10 +5,7 @@
 
 from adlib.adversaries.adversary import Adversary
 from data_reader.binary_input import Instance
-import cvxpy as cvx
 import numpy as np
-from copy import deepcopy
-from progress.bar import Bar
 from typing import List, Dict
 
 
@@ -18,12 +15,84 @@ from typing import List, Dict
 
 
 class KInsertion(Adversary):
-    def __init__(self):
+    def __init__(self, learner, poison_instance, alpha, beta):
         Adversary.__init__(self)
+        self.learner = learner
+        self.poison_instance = poison_instance
+        self.alpha = alpha
+        self.beta = beta
 
     def attack(self, instances) -> List[Instance]:
-        raise NotImplementedError
+        if len(instances) == 0:
+            raise ValueError('Need at least one instance.')
+        # TODO: Implement
 
+    def _kernel_linear(self, fv_1: np.ndarray, fv_2: np.ndarray):
+        """
+        Returns the value of the specified kernel function
+        :param fv_1: feature vector 1 (np.ndarray)
+        :param fv_2: feature vector 2 (np.ndarray)
+        :return: the value of the specified kernel function
+        """
+
+        if len(fv_1) != len(fv_2):
+            raise ValueError('Feature vectors need to have same length.')
+
+        return fv_1.dot(fv_2)
+
+    def _kernel_derivative_linear(self, fv_1: np.ndarray,
+                                  fv_2: np.ndarray, k: int):
+        """
+        Returns the value of the derivative of the specified kernel function
+        with fv_2 being the variable (i.e. K(x_i, x_c), finding gradient
+        evaluated at x_c
+        :param fv_1: fv_1: feature vector 1 (np.ndarray)
+        :param fv_2: fv_2: feature vector 2 (np.ndarray)
+        :param k: which partial derivative (0-based indexing, int)
+        :return: the value of the derivative of the specified kernel function
+        """
+
+        if len(fv_1) != len(fv_2) or k < 0 or k >= len(fv_1):
+            raise ValueError('Feature vectors need to have same '
+                             'length and k must be a valid index.')
+
+        return fv_1[k]
+
+    def _kernel_poly(self, fv_1: np.ndarray, fv_2: np.ndarray):
+        """
+        Returns the value of the specified kernel function
+        :param fv_1: feature vector 1 (np.ndarray)
+        :param fv_2: feature vector 2 (np.ndarray)
+        :return: the value of the specified kernel function
+        """
+
+        if len(fv_1) != len(fv_2):
+            raise ValueError('Feature vectors need to have same length.')
+
+        return ((self.learner.gamma * fv_1.dot(fv_2) + self.learner.coef0) **
+                self.learner.degree)
+
+    def _kernel_derivative_poly(self, fv_1: np.ndarray,
+                                fv_2: np.ndarray, k: int):
+        """
+        Returns the value of the derivative of the specified kernel function
+        with fv_2 being the variable (i.e. K(x_i, x_c), finding gradient
+        evaluated at x_c
+        :param fv_1: fv_1: feature vector 1 (np.ndarray)
+        :param fv_2: fv_2: feature vector 2 (np.ndarray)
+        :param k: which partial derivative (0-based indexing, int)
+        :return: the value of the derivative of the specified kernel function
+        """
+
+        if len(fv_1) != len(fv_2) or k < 0 or k >= len(fv_1):
+            raise ValueError('Feature vectors need to have same '
+                             'length and k must be a valid index.')
+
+        return (fv_1[k] * self.learner.degree *
+                self.learner.gamma *
+                ((self.learner.gamma * fv_1.dot(fv_2) + self.learner.coef0) **
+                 (self.learner.degree - 1)))
+    
     def set_params(self, params: Dict):
         raise NotImplementedError
 
---
+++ adlib/tests/adversaries/k_insertion_test.py
@@ -0,0 +1,24 @@
+# k_insertion_test.py
+# Tests the k-insertion implementation
+# Matthew Sedam
+
+from sklearn import svm
+from adlib.learners import SimpleLearner
+import numpy as np
+from data_reader.dataset import EmailDataset
+from data_reader.operations import load_dataset
+from adlib.adversaries.k_insertion import KInsertion
+
+
+def test_k_insertion():
+    # Data processing unit
+    # The path is an index of 400 testing samples(raw email data).
+    dataset = EmailDataset(path='./data_reader/data/raw/trec05p-1/test-400',
+                           binary=True, raw=True)
+    training_data = load_dataset(dataset)
+
+    # Setting the default learner
+    # Test simple learner svm
+    learning_model = svm.SVC(probability=True, kernel='linear')
+    learner = SimpleLearner(learning_model, training_data)
+    learner.train()

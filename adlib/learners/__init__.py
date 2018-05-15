c479a1f03d26156d932250ad279460d70ce6ea08
Matthew Sedam <sedammatthew@gmail.com>
2018-05-15 10:07:51 -0500

    Revert "Removed __init__.py as they are no longer needed with Python 3.3+"
    This reverts commit 9229bc6dab0160d280b01623aebec4c0db698959.

---
+++ adlib/learners/__init__.py
@@ -0,0 +1,6 @@
+from .learner import learner
+from .feature_deletion import FeatureDeletion
+from .retraining import Retraining
+from .simple_learner import SimpleLearner
+from .svm_restrained import SVMRestrained
+from .svm_freerange import SVMFreeRange
---
+++ data_reader/__init__.py
@@ -0,0 +1,4 @@
+import scipy
+# import data_reader
+# from data_reader import input
+# from data_reader import output

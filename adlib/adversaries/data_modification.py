Polish data-modification; fix formula error

adlib/adversaries/data_modification.py
@@ -12,11 +12,8 @@ import numpy as np
 import pathos.multiprocessing as mp
 
 
-# TODO: Possibly bad formula!!!!!!!!!!!!!!
-
-
 class DataModification(Adversary):
-    def __init__(self, learner, target_theta, alpha=1e-3, beta=0.1,
+    def __init__(self, learner, target_theta, alpha=1e-3, beta=0.05,
                  verbose=False):
 
         Adversary.__init__(self)
@@ -40,17 +37,28 @@ class DataModification(Adversary):
 
         self.instances = instances
         self._calculate_constants(instances)
+        self._fuzz_fvs()
 
-        dist = self.alpha * 2
-
+        dist = np.linalg.norm(self.theta - self.target_theta)
+        iteration = 0
         while dist > self.alpha:
+            if self.verbose:
+                print('Distance (iteration: ', iteration, '): ', dist, sep='')
+
             gradient = self._calc_gradient()
             self.fvs += gradient
             self._calc_theta()
-
             dist = np.linalg.norm(self.theta - self.target_theta)
-            if self.verbose:
-                print('Distance: ', dist, '\n', sep='')
+            iteration += 1
+
+    def _fuzz_fvs(self):
+        """
+        Add to every entry of self.fvs some noise to make it non-singular.
+        """
+
+        for i in range(len(self.instances)):
+            for j in range(self.instances[0].get_feature_count()):
+                self.fvs[i][j] += np.random.normal(0, 0.1)
 
     def _calculate_constants(self, instances: List[Instance]):
         # Calculate feature vectors as np.ndarrays
@@ -65,7 +73,7 @@ class DataModification(Adversary):
                     tmp.append(0)
             tmp = np.array(tmp)
             self.fvs.append(tmp)
-        self.fvs = np.array(self.fvs)
+        self.fvs = np.array(self.fvs, dtype='float64')
         self.orig_fvs = deepcopy(self.fvs)
 
         # Calculate labels
@@ -94,7 +102,13 @@ class DataModification(Adversary):
     def _calc_gradient(self):
         matrix_1 = self._calc_partial_f_partial_capital_d()
         matrix_2 = self._calc_partial_f_partial_theta()
-        matrix_2 = np.linalg.inv(matrix_2)
+
+        try:
+            matrix_2 = np.linalg.inv(matrix_2)
+        except np.linalg.linalg.LinAlgError:
+            # Singular matrix -> do not move values with this part of gradient
+            matrix_2 = np.full(matrix_2.shape, 0)
+
         partial_theta_partial_capital_d = -1 * matrix_1.dot(matrix_2)
 
         # Calculate first part
@@ -139,15 +153,13 @@ class DataModification(Adversary):
         return np.array(matrix)
 
     def _calc_partial_f_j_partial_x_k(self, j, k):
-        # TODO: Possibly bad formula!!!!!!!!!!!!!!
         running_sum = 0
         for i in range(len(self.instances)):
             val = self.logistic_vals[i]
-            running_sum += (val * (1 - val) * self.labels[i] * self.fvs[i][j] *
-                            self.theta[k])
-            running_sum -= 1
+            inside = val * self.fvs[i][j] * self.theta[k]
             if j == k:
-                running_sum += val * self.labels[i]
+                inside -= 1
+            running_sum += (1 - val) * self.labels[i] * inside
 
         return running_sum
 

adlib/tests/adversaries/data_modification_test.py
@@ -7,6 +7,7 @@ from adlib.learners import SimpleLearner
 from adlib.adversaries.data_modification import DataModification
 from adlib.tests.adversaries.label_flipping_test import \
     calculate_correct_percentages
+from copy import deepcopy
 from data_reader.dataset import EmailDataset
 from data_reader.operations import load_dataset
 from sklearn import svm
@@ -25,8 +26,8 @@ def test_data_modification():
                            binary=True, raw=True)
     training_data = load_dataset(dataset)
 
-    # Randomly choose ~20% of dataset to decrease debugging time
-    choices = np.random.binomial(1, 0.2, len(training_data))
+    # Randomly choose ~0.7% of dataset to decrease debugging time
+    choices = np.random.binomial(1, 0.007, len(training_data))
     temp = []
     predict_data = []
     count = 0
@@ -45,11 +46,46 @@ def test_data_modification():
     orig_learner = SimpleLearner(orig_learning_model, training_data)
     orig_learner.train()
 
+    ############################################################################
+    # Calculate target theta, 1 -> spam, -1 -> ham; For the target theta
+    # calculation, I am assuming I know which spam I want to be classified
+    # as ham and the features I want to have a disproportionate effect on the
+    # decision function calculation. For example, if feature #32 is something
+    # that all of my spam has in common, I want to make the entry corresponding
+    # to #32 (index 32 - 1 = 31) in target_theta to be disproportionately
+    # negative so that when my spam is being classified, the 1 indicating that
+    # feature #32 is present will be multiplied by a large negative number so as
+    # to decrease the value of the decision function and hopefully make it
+    # negative so as to classify my spam as ham.
+
+    lnr = orig_learner.model.learner
+    eye = np.eye(training_data[0].get_feature_count(), dtype=int)
+    orig_theta = lnr.decision_function(eye) - lnr.intercept_[0]
+    target_theta = deepcopy(orig_theta)
+
+    spam_instances = []
+    for inst in training_data:
+        if inst.get_label() == 1:
+            spam_instances.append(inst)
+
+    features = get_spam_features(spam_instances)
+    mean = np.mean(orig_theta)
+    std = np.std(orig_theta)
+
+    # Set features to mean + 3 STD and make it negative to help it be classified
+    # as ham and NOT spam
+    value = -1 * (mean + 3 * std)
+
+    for index in features:
+        target_theta[index] = value
+
+    ############################################################################
+
     # Get original predictions
     original_pred_labels = orig_learner.predict(training_data)
 
     # Do the attack
-    attacker = DataModification(orig_learner, True)
+    attacker = DataModification(orig_learner, target_theta, verbose=True)
     attack_data = attacker.attack(training_data)
 
     # Retrain the model with poisoned data
@@ -94,6 +130,30 @@ def test_data_modification():
 
     print('\nEND data modification attack.')
     print('#################################################################\n')
+
+
+def get_spam_features(instances, p=0.75):
+    """
+    Returns a list of feature indices where the proportion of instances that
+    have them is >= p
+    :param instances: the spam instances - MUST BE SPAM (i.e. have a label of 1)
+    :param p: the proportion of instances that must have this value
+    :return: the list of feature indices
+    """
+
+    if len(instances) == 0:
+        raise ValueError('Must have at least one instance.')
+
+    features = []
+    for i in range(instances[0].get_feature_count()):
+        count = 0
+        for inst in instances:
+            count += 1 if inst.get_feature_vector().get_feature(i) == 1 else 0
+
+        if (count / len(instances)) >= p:
+            features.append(i)
+
+    return features
 
 
 if __name__ == '__main__':
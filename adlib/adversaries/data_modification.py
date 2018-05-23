b30622397eae07060908bee1d424a96f9c3b8026
Matthew Sedam <sedammatthew@gmail.com>
2018-05-23 15:10:29 -0500

    Tweaked gradient projection code for data-modification

--- adlib/adversaries/data_modification.py
+++ adlib/adversaries/data_modification.py
@@ -12,7 +12,6 @@ import numpy as np
 import pathos.multiprocessing as mp
 
 
-# TODO: Fix singular matrix error
 # TODO: Verify implementation
 
 
@@ -79,6 +78,16 @@ class DataModification(Adversary):
         distance = max_val - min_val
         transformation = lambda x: (x - min_val) / distance
 
+        # If moving away from 0 or one in the outward direction, flip the value
+        # 0 -> 1, 1 -> 0
+        for i in range(len(self.instances)):
+            for j in range(self.instances[0].get_feature_count()):
+                if self.fvs[i][j] < 0:
+                    self.fvs[i][j] += 1
+                elif self.fvs > 1:
+                    self.fvs[i][j] -= 1
+
+        # Transform [a, b] to [0, 1]
         for i in range(len(self.instances)):
             for j in range(self.instances[0].get_feature_count()):
                 self.fvs[i][j] = transformation(self.fvs[i][j])

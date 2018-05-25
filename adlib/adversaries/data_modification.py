360c5a4f2245dceeb10ae6b008466b385e75cff7
Matthew Sedam <sedammatthew@gmail.com>
2018-05-25 10:49:55 -0400

    data-modification: Fix f/D

--- adlib/adversaries/data_modification.py
+++ adlib/adversaries/data_modification.py
@@ -178,10 +178,10 @@ class DataModification(Adversary):
 
         return running_sum
 
-    def _calc_partial_f_partial_capital_d(self):
+    def _calc_partial_f_partial_capital_d(self, i):
         pool = mp.Pool(mp.cpu_count())
         matrix = pool.map(lambda j: list(map(
-            lambda k: self._calc_partial_f_j_partial_x_k(j, k),
+            lambda k: self._calc_partial_f_j_partial_x_k(i, j, k),
             range(len(self.theta)))), range(len(self.theta)))
         pool.close()
         pool.join()

217ec5d8e7b9a5c7d445111ba25f2a0d454f4395
Matthew Sedam <sedammatthew@gmail.com>
2018-06-04 17:33:44 -0500

    TRIM learner: Add loss calculation

--- adlib/learners/trim_learner.py
+++ adlib/learners/trim_learner.py
@@ -4,8 +4,11 @@
 # Learning" found at https://arxiv.org/pdf/1804.00308.pdf.
 # Matthew Sedam
 
+from data_reader.binary_input import Instance
 from adlib.learners.learner import learner
-from typing import Dict
+from adlib.learners.simple_learner import SimpleLearner
+from typing import Dict, List
+import numpy as np
 
 
 class TRIM_Learner(learner):
@@ -14,16 +17,58 @@ class TRIM_Learner(learner):
     mentioned above.
     """
 
-    def __init__(self):
+    def __init__(self, base_learner: SimpleLearner, n: int, lda=0.1):
         learner.__init__(self)
-        raise NotImplementedError
+        self.base_learner = base_learner
+        self.n = n
+        self.lda = lda  # lambda
+        self.training_instances = base_learner.training_instances
+        self.num_features = base_learner.num_features
 
     def train(self):
         """
         Train on the set of training instances.
         """
 
-        raise NotImplementedError
+        self.base_learner.train()
+
+        # Create random sample of size self.n
+        inst_set = []
+        while len(inst_set) < self.n:
+            for inst in self.training_instances:
+                if np.random.binomial(1, 0.5) == 1 and len(inst_set) < self.n:
+                    inst_set.append(inst)
+
+                if len(inst_set) == self.n:
+                    break
+
+    def _calc_loss(self, inst_set: List[Instance]):
+        """
+        Calculates the loss function as specified in the paper
+        :param inst_set: the set of Instances
+        :return: the loss
+        """
+
+        w = self.base_learner.model.learner.decision_function(
+            np.eye(self.training_instances[0].get_feature_count()))
+        w -= self.base_learner.model.learner.intercept_[0]
+        loss = 0.5 * self.lda * (np.linalg.norm(w) ** 2)
+
+        # Calculate feature vector matrix and label array
+        fvs = []
+        labels = []
+        for inst in inst_set:
+            fvs.append(TRIM_Learner.get_feature_vector_array(inst))
+            labels.append(inst.get_label())
+        fvs = np.array(fvs)
+        labels = np.array(labels)
+
+        # Calculate loss
+        f_values = self.base_learner.model.learner.decision_function(fvs)
+        tmp = np.array(list(map(lambda x: x ** 2, f_values - labels)))
+        loss += (1 / self.n) * sum(tmp)
+
+        return loss
 
     def predict(self, instances):
         """
@@ -47,3 +92,20 @@ class TRIM_Learner(learner):
 
     def decision_function(self, X):
         raise NotImplementedError
+
+    @staticmethod
+    def get_feature_vector_array(inst: Instance):
+        """
+        Turns the feature vector into an np.ndarray
+        :param inst: the Instance
+        :return: the feature vector (np.ndarray)
+        """
+
+        fv = inst.get_feature_vector()
+        tmp = []
+        for j in range(inst.get_feature_count()):
+            if fv.get_feature(j) == 1:
+                tmp.append(1)
+            else:
+                tmp.append(0)
+        return np.array(tmp)

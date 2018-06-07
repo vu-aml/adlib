Add gradient descent attack and fix feature deletion for further testing

combats/FD_FD.py
@@ -1,10 +1,11 @@
 import sys
+sys.path.append("/home/dingx/adlib/adlib")
 from sklearn import svm
 from learners import FeatureDeletion
 from timeit import default_timer as timer
 from data_reader.dataset import EmailDataset
 from data_reader.operations import load_dataset
-from adversaries.coordinate_greedy import CoordinateGreedy
+from adversaries.feature_deletion import AdversaryFeatureDeletion
 from sklearn import metrics
 from utils import save_result
 import multiprocessing
@@ -34,8 +35,8 @@ def single_run_list(y_pred, y_true):
     return result
 
 def run(par_map):
-    _dataset = EmailDataset(path='../data_reader/data/uci/uci_modified.csv', binary=False, raw=False)
-    train_,test_ = _dataset.split(0.2)
+    _dataset = EmailDataset(path='../data_reader/data/enron/10', binary=False, raw=True, max_features_=500)
+    train_,test_ = _dataset.split(0.7)
     training_data = load_dataset(train_)
     testing_data = load_dataset(test_)
     test_true_label = [x.label for x in testing_data]
@@ -54,7 +55,7 @@ def run(par_map):
 
     #test Restrained_attack
     a_start = timer()
-    attacker = CoordinateGreedy()
+    attacker = AdversaryFeatureDeletion()
     attacker.set_params(par_map)
     attacker.set_adversarial_params(fd_learner, testing_data)
     attacked_data = attacker.attack(testing_data)
@@ -87,10 +88,11 @@ def generate_interval(start, end, process_count, dtype=None, log=False):
 
 if __name__ == '__main__':
     param_path = sys.argv[1]
-    lst = generate_param_map(param_path,process_time=1)
-    pool = multiprocessing.Pool(processes=1)
+    return_path = sys.argv[2]
+    lst = generate_param_map(param_path,process_time=5)
+    pool = multiprocessing.Pool(processes=5)
     result = pool.map(run, lst)
     arr = np.array(result)
     data = pd.DataFrame(arr, columns = ["old_acc", "old_prec", "old_rec", "old_f1","learn_t",
                 "new_acc", "new_prec","new_rec", "new_f1","atk_t"])
-    data.to_csv("result2.csv", sep='\t', encoding='utf-8')
+    data.to_csv(return_path, sep='\t', encoding='utf-8')

data_reader/dataset.py
@@ -231,18 +231,26 @@ class EmailDataset(Dataset):
                     serialize.writerow(instance)
         else:
             # TODO: throw exception if FileNotFoundError
-            data = np.genfromtxt(outfile, delimiter=',')
-            self.num_instances = data.shape[0]
-            labels = data[:, :1]
-            feats = data[:, 1:]
-            mask = ~np.isnan(feats)
-            col = feats[mask]
-            row = np.concatenate([np.ones_like(x)*i
-                                 for i, x in enumerate(feats)])[mask.flatten()]
-            features = csr_matrix((np.ones_like(col), (row, col)),
-                                  shape=(feats.shape[0],
-                                  int(np.max(feats[mask]))+1))
-            return np.squeeze(labels), features
+            if self.binary:
+                data = np.genfromtxt(outfile, delimiter=',')
+                self.num_instances = data.shape[0]
+                labels = data[:, :1]
+                feats = data[:, 1:]
+                mask = ~np.isnan(feats)
+                col = feats[mask]
+                row = np.concatenate([np.ones_like(x) * i
+                                      for i, x in enumerate(feats)])[mask.flatten()]
+                features = csr_matrix((np.ones_like(col), (row, col)),
+                                      shape=(feats.shape[0],
+                                             int(np.max(feats[mask])) + 1))
+                return np.squeeze(labels), features
+            else:
+                data = np.genfromtxt(outfile, delimiter=',')
+                self.num_instances = data.shape[0]
+                labels = data[:, :1]
+                feats = data[:, 1:]
+                features = csr_matrix(feats)
+                return np.squeeze(labels), features
 
     def _pickle(self, outfile, save=True):
         """A fast method for saving and loading datasets as python objects.

learners/feature_deletion.py
@@ -32,13 +32,14 @@ class FeatureDeletion(learner):
                   'max_feature_deletion': self.max_feature_deletion}
         return params
 
+
     def train(self):
         """
-        Opitimize weight vector using FDROP algorithm
-        i.e. formula (6) described in Globerson and Roweis paper
-        Returns: optimized weight vector
+         Opitimize weight vector using FDROP algorithm
+         i.e. formula (6) described in Globerson and Roweis paper
+         Returns: optimized weight vector
 
-        """
+         """
         if isinstance(self.training_instances, List):
             y_list, X_list = sparsify(self.training_instances)
             num_instances = len(y_list)
@@ -50,43 +51,59 @@ class FeatureDeletion(learner):
             num_instances = len(y)
             y, X = y.reshape((num_instances, 1)), X
 
+        #append another column at X for bias
+        bias_col = np.ones_like(y.T)
+        X_prime = np.insert(X, X.shape[1], bias_col, axis=1)
+
         C = self.hinge_loss_multiplier
         print("current C value(hinge loss multipler): {}".format(C))
         print("current K(maximum feature deletion): {}".format(self.max_feature_deletion))
         print(X.shape)
+        print(y.shape)
         K = self.max_feature_deletion
-
-        w = Variable(self.num_features)  # weight vector
-        b = Variable()  # bias term
+        w = Variable(self.num_features + 1)  # weight vector
+        #b = Variable()  # bias term
         t = Variable(num_instances)
         z = Variable(num_instances)
-        v = Variable(num_instances, self.num_features)
+        v = Variable(num_instances, (self.num_features + 1))
         loss_f = Variable(num_instances)
 
-        #loss = sum_entries(pos(1 - mul_elemwise(y, X * w + b) + t))
+        #bias is implemented as the last column of X(a extra feature vector of only 1)
+        #bias in the weight vector is not calculated in the regularization
 
-        constraints = [t >= K * z + sum_entries(v, axis=1),
-                       v >= 0]
+        constraints = [t >= K * z + sum_entries(v, axis=1), v >= 0]
         constraints.extend([z[i] + v[i, :] >=
-                            y[i] * mul_elemwise(X[i], w).T for i in range(num_instances)])
+                            y[i] * mul_elemwise(X_prime[i], w).T for i in range(num_instances)])
         constraints.extend([loss_f[i] >= 0 for i in range(num_instances)])
-        constraints.extend([loss_f[i] >= (1 - mul_elemwise(y[i], X[i] * w + b) + t[i]) for i in range(num_instances)])
-        obj = Minimize(0.5 * (sum_squares(w)) + C * sum_entries(loss_f))
+        constraints.extend([loss_f[i] >= (1 - y[i] * (X_prime[i] * w) + t[i]) for i in range(num_instances)])
+        obj = Minimize(0.5 * (sum_squares(w[:-1])) + C * sum_entries(loss_f))
+
+        #  constraints = [t >= K * z + sum_entries(v, axis=1),v >= 0]
+        #  constraints.extend([z[i] + v[i, :] >=
+        #                      y[i] * mul_elemwise(X[i], w) for i in range(num_instances)])
+        #  constraints.extend([])
+        #  constraints.extend([loss_f[i] >= 0 for i in range(num_instances)])
+        #  constraints.extend([loss_f[i] >= (1 - y[i] * (X[i] * w + b) + t[i]) for i in range(num_instances)])
+        #  obj = Minimize(0.5 * (sum_squares(w)) + C * sum_entries(loss_f))
 
         prob = Problem(obj, constraints)
-        #switch another server to solve the scalability issue
+        # switch another server to solve the scalability issue
         prob.solve(solver=SCS)
         # print("training completed, here is the learned weight vector:")
-
         # weight_vector is of shape (1, self.num_features)
-        self.weight_vector = [np.array(w.value).T][0][0]
-        self.bias = b.value
+        self.weight_vector = [np.array(w.value).T][0][0][:-1]
+        self.bias = [np.array(w.value).T][0][0][-1]
+        self.t = t
+        print(self.weight_vector)
+        print(self.bias)
+
         print("final weight vector shape: {}".format(self.weight_vector.shape))
-       # print("bias term:{}".format(self.bias))
+        # print("bias term:{}".format(self.bias))
         top_idx = [i for i in np.argsort(np.absolute(self.weight_vector))[-10:]]
         print("indices with top 10 absolute value:")
         for i in top_idx:
             print("index No.{} with value {}".format(i, self.weight_vector[i]))
+
 
     def predict(self, instances):
         """
@@ -123,8 +140,11 @@ class FeatureDeletion(learner):
         '''
         return self.weight_vector.dot(features.T)[0] + self.bias
 
-    def decision_function(self):
-        return self.weight_vector, self.bias
+    #decision_function should be the distance to the hyperplane
+    def decision_function(self, instances):
+        predict_instances = self.weight_vector.dot(instances.T) + self.bias
+        norm = np.linalg.norm(self.weight_vector)
+        return predict_instances / norm
 
     def get_weight(self):
         return self.weight_vector

learners/svm_freerange.py
@@ -142,19 +142,18 @@ class SVMFreeRange(learner):
                 return predictions[0]
         return predictions
 
-
-    def predict_instance(self, features: np.array):
-        return self.weight_vector.dot(features.T)[0] + self.bias
-
     def predict_proba(self, instances):
         return self.predict(instances)
 
-    #correct???
+    def predict_instance(self, instances: np.array):
+        return self.weight_vector.dot(instances.T)[0] + self.bias
+
     #decision_function should be the distance to the hyperplane
-    def decision_function(self):
-        #determinant = np.linalg.det(self.weight_vector)
-        #return (self.weight_vector / determinant) * instance.T + self.bias / determinant
-        return self.weight_vector, self.bias
+    def decision_function(self, instances):
+        predict_instances = self.weight_vector.dot(instances.T) + self.bias
+        norm = np.linalg.norm(self.weight_vector)
+        return predict_instances / norm
+
 
     def get_weight(self):
         return self.weight_vector

learners/svm_restrained.py
@@ -129,15 +129,18 @@ class SVMRestrained(learner):
                 return predictions[0]
         return predictions
 
-    def predict_instance(self, features: np.array):
-        return self.weight_vector.dot(features.T)[0] + self.bias
-
     def predict_proba(self, instances):
 
         return self.predict(instances)
 
-    def decision_function(self):
-        return self.weight_vector, self.bias
+    def predict_instance(self, instances: np.array):
+        return self.weight_vector.dot(instances.T)[0] + self.bias
+
+    #decision_function should be the distance to the hyperplane
+    def decision_function(self, instances):
+        predict_instances = self.weight_vector.dot(instances.T) + self.bias
+        norm = np.linalg.norm(self.weight_vector)
+        return predict_instances / norm
 
     def get_weight(self):
         print("weight vec shape returned from Restrained learner: {}".format(self.weight_vector.shape))
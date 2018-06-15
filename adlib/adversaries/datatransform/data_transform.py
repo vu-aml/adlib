99552792ee0788ac8eb3074de7c0c6c15838bde4
Matthew Sedam <sedammatthew@gmail.com>
2018-06-15 14:02:43 -0500

    data-transformation: Finish implementation

--- adlib/adversaries/datatransform/data_transform.py
+++ adlib/adversaries/datatransform/data_transform.py
@@ -5,30 +5,9 @@
 # Matthew Sedam. 2018. Original code from Matthew Jagielski.
 
 from adlib.adversaries import Adversary
-from adlib.adversaries.datatransform.poisoning.poison import main
-from data_reader.binary_input import Instance
-from typing import Dict, List
-import argparse
-
-""" Examples
-GD with random flipping
-python MYTEST -m ridge -init randflip -obj 0 -a 0.5 -b 0.1 -i 1 -s 4 -seed 123
-
-BGD with AlfaTilt
-python MYTEST -m ridge -init alfatilt -obj 0 -a 0.5 -b 0.1 -i 1 -s 4 -seed 123
-
-BGD with InfFlip
-python MYTEST -m ridge -init inflip -obj 0 -a 0.5 -b 0.1 -i 1 -s 4 -seed 123
-
-BGD with Adaptive
-python MYTEST -m ridge -init adaptive -obj 0 -a 0.5 -b 0.1 -i 1 -s 4 -seed 123
-
-Validation with random flipping
-python MYTEST -m ridge -init randflip -obj 1 -a 0.5 -b 0.1 -i 1 -s 4 -seed 123
-
-New objective with random flipping
-python MYTEST -m ridge -init randflip -obj 2 -a 0.5 -b 0.1 -i 1 -s 4 -seed 123
-"""
+from adlib.adversaries.datatransform.poisoning.poison import *
+from typing import Dict
+from argparse import Namespace
 
 
 class DataTransform(Adversary):
@@ -40,6 +19,7 @@ class DataTransform(Adversary):
                  numinit=1, objective=1, optimizey=False, partct=4, poisct=75,
                  rounding=False, seed=123, sigma=1.0, testct=500, trainct=300,
                  validct=250, visualize=False):
+
         Adversary.__init__(self)
         self.beta = beta
         self.dataset = dataset
@@ -65,11 +45,202 @@ class DataTransform(Adversary):
         self.visualize = visualize
         self.args = self.get_available_params()
 
-    def attack(self, instances) -> List[Instance]:
-        main(argparse.Namespace(**self.args))
+    def attack(self, instances):
+        x = instances[0]
+        y = instances[1]
+        args = Namespace(**self.args)
+
+        colmap = {}
+        trainfile, testfile, validfile, resfile, newlogdir = \
+            open_logging_files(args.logdir, args.model, args.logind, args)
+        trainx, trainy, testx, testy, poisx, poisy, validx, validy = \
+            sample_dataset(x, y, args.trainct, args.poisct, args.testct,
+                           args.validct, args.seed)
+
+        for i in range(len(testy)):
+            testfile.write(','.join(
+                [str(val) for val in [testy[i]] + testx[i].tolist()[0]]) + '\n')
+        testfile.close()
+
+        for i in range(len(validy)):
+            validfile.write(','.join(
+                [str(val) for val in
+                 [validy[i]] + validx[i].tolist()[0]]) + '\n')
+        validfile.close()
+
+        for i in range(len(trainy)):
+            trainfile.write(','.join(
+                [str(val) for val in
+                 [trainy[i]] + trainx[i].tolist()[0]]) + '\n')
+
+        print(la.matrix_rank(trainx))
+        print(trainx.shape)
+
+        totprop = args.poisct / (args.poisct + args.trainct)
+        print(totprop)
+
+        timestart, timeend = None, None
+        types = {'linreg': LinRegGDPoisoner,
+                 'lasso': LassoGDPoisoner,
+                 'enet': ENetGDPoisoner,
+                 'ridge': RidgeGDPoisoner}
+
+        inits = {'levflip': levflip,
+                 'cookflip': cookflip,
+                 'alfatilt': alfa_tilt,
+                 'inflip': inf_flip,
+                 'ffirst': farthestfirst,
+                 'adaptive': adaptive,
+                 'randflip': randflip,
+                 'randflipnobd': randflipnobd,
+                 'rmml': rmml}
+
+        bestpoisx, bestpoisy, besterr = None, None, -1
+
+        init = inits[args.initialization]
+
+        genpoiser = types[args.model](trainx, trainy, testx, testy, validx,
+                                      validy,
+                                      args.eta, args.beta, args.sigma,
+                                      args.epsilon,
+                                      args.multiproc,
+                                      trainfile, resfile, args.objective,
+                                      args.optimizey, colmap)
+
+        for initit in range(args.numinit):
+            poisx, poisy = init(trainx, trainy,
+                                int(args.trainct * totprop / (
+                                        1 - totprop) + 0.5))
+            clf, _ = genpoiser.learn_model(
+                np.concatenate((trainx, poisx), axis=0),
+                trainy + poisy, None)
+            err = genpoiser.computeError(clf)[0]
+            print('Validation Error:', err)
+            if err > besterr:
+                bestpoisx, bestpoisy, besterr = np.copy(poisx), poisy[:], err
+        poisx, poisy = np.matrix(bestpoisx), bestpoisy
+        poiser = types[args.model](trainx, trainy, testx, testy, validx, validy,
+                                   args.eta, args.beta, args.sigma,
+                                   args.epsilon,
+                                   args.multiproc, trainfile, resfile,
+                                   args.objective, args.optimizey, colmap)
+
+        for i in range(args.partct + 1):
+            curprop = (i + 1) * totprop / (args.partct + 1)
+            numsamples = int(0.5 + args.trainct * (curprop / (1 - curprop)))
+            curpoisx = poisx[:numsamples, :]
+            curpoisy = poisy[:numsamples]
+            trainfile.write('\n')
+
+            timestart = datetime.datetime.now()
+            poisres, poisresy = poiser.poison_data(curpoisx, curpoisy,
+                                                   timestart,
+                                                   args.visualize, newlogdir)
+            print(poisres.shape, trainx.shape)
+            poisedx = np.concatenate((trainx, poisres), axis=0)
+            poisedy = trainy + poisresy
+
+            clfp, _ = poiser.learn_model(poisedx, poisedy, None)
+            clf = poiser.initclf
+            if args.rounding:
+                roundx, roundy = roundpois(poisres, poisresy)
+                rpoisedx, rpoisedy = np.concatenate((trainx, roundx),
+                                                    axis=0), trainy + roundy
+                clfr, _ = poiser.learn_model(rpoisedx, rpoisedy, None)
+                rounderr = poiser.computeError(clfr)
+
+            errgrd = poiser.computeError(clf)
+            err = poiser.computeError(clfp)
+
+            timeend = datetime.datetime.now()
+
+            towrite = [numsamples, -1, None, None, err[0], err[1],
+                       (timeend - timestart).total_seconds()]
+            resfile.write(','.join([str(val) for val in towrite]) + '\n')
+            trainfile.write('\n')
+            for j in range(numsamples):
+                trainfile.write(','.join([str(val) for val in
+                                          [poisresy[j]] +
+                                          poisres[j].tolist()[0]]) + '\n')
+
+            if args.rounding:
+                towrite = [numsamples, 'r', None, None, rounderr[0],
+                           rounderr[1],
+                           (timeend - timestart).total_seconds()]
+                resfile.write(','.join([str(val) for val in towrite]) + '\n')
+                trainfile.write('\nround\n')
+                for j in range(numsamples):
+                    trainfile.write(','.join([str(val) for val in
+                                              [roundy[j]] +
+                                              roundx[j].tolist()[0]]) + '\n')
+
+            resfile.flush()
+            trainfile.flush()
+            os.fsync(resfile.fileno())
+            os.fsync(trainfile.fileno())
+
+        trainfile.close()
+        testfile.close()
+
+        print()
+        print('Unpoisoned')
+        print('Validation MSE:', errgrd[0])
+        print('Test MSE:', errgrd[1])
+        print('Poisoned:')
+        print('Validation MSE:', err[0])
+        print('Test MSE:', err[1])
+        if args.rounding:
+            print('Rounded')
+            print('Validation MSE', rounderr[0])
+            print('Test MSE:', rounderr[1])
+
+        return poisedx, poisedy
 
     def set_params(self, params: Dict):
-        raise NotImplementedError
+        if params['beta'] is not None:
+            self.beta = params['beta']
+        if params['dataset'] is not None:
+            self.dataset = params['dataset']
+        if params['epsilon'] is not None:
+            self.epsilon = params['epsilon']
+        if params['eta'] is not None:
+            self.eta = params['eta']
+        if params['initialization'] is not None:
+            self.initialization = params['initialization']
+        if params['lambd'] is not None:
+            self.lambd = params['lambd']
+        if params['logdir'] is not None:
+            self.logdir = params['logdir']
+        if params['logind'] is not None:
+            self.logind = params['logind']
+        if params['model'] is not None:
+            self.model = params['model']
+        if params['multiproc'] is not None:
+            self.multiproc = params['multiproc']
+        if params['numinit'] is not None:
+            self.numinit = params['numinit']
+        if params['objective'] is not None:
+            self.objective = params['objective']
+        if params['optimizey'] is not None:
+            self.optimizey = params['optimizey']
+        if params['partct'] is not None:
+            self.partct = params['partct']
+        if params['poisct'] is not None:
+            self.poisct = params['poisct']
+        if params['rounding'] is not None:
+            self.rounding = params['rounding']
+        if params['seed'] is not None:
+            self.seed = params['seed']
+        if params['sigma'] is not None:
+            self.sigma = params['sigma']
+        if params['testsct'] is not None:
+            self.testct = params['testct']
+        if params['trainct'] is not None:
+            self.trainct = params['trainct']
+        if params['validct'] is not None:
+            self.validct = params['validct']
+        if params['visualize'] is not None:
+            self.visualize = params['visualize']
 
     def get_available_params(self):
         params = {'beta': self.beta,
@@ -98,4 +269,4 @@ class DataTransform(Adversary):
         return params
 
     def set_adversarial_params(self, learner, train_instances):
-        raise NotImplementedError
+        pass
--- adlib/tests/adversaries/data_transform_test.py
+++ adlib/tests/adversaries/data_transform_test.py
@@ -3,6 +3,7 @@
 # Matthew Sedam
 
 from adlib.adversaries.datatransform.data_transform import DataTransform
+from adlib.adversaries.datatransform.poisoning.poison import open_dataset
 
 
 def test_data_transform():
@@ -10,8 +11,34 @@ def test_data_transform():
     print('###################################################################')
     print('START data transform attack.\n')
 
-    attacker = DataTransform()
-    attacker.attack(None)
+    args = {'beta': 0.1,
+            'dataset': ('./data_reader/data/raw/data-transform/'
+                        'house-processed.csv'),
+            'epsilon': 0.001,
+            'eta': 0.5,
+            'initialization': 'randflip',
+            'lambd': 1,
+            'logdir': './results',
+            'logind': 0,
+            'model': 'ridge',
+            'multiproc': False,
+            'numinit': 1,
+            'objective': 1,
+            'optimizey': False,
+            'partct': 4,
+            'poisct': 75,
+            'rounding': False,
+            'seed': 123,
+            'sigma': 1.0,
+            'testct': 500,
+            'trainct': 300,
+            'validct': 250,
+            'visualize': False}
+
+    x, y = open_dataset(args['dataset'], args['visualize'])
+
+    attacker = DataTransform(**args)
+    poisoned_x, poisoned_y = attacker.attack((x, y))
 
     print('\nEND data transform attack.')
     print('###################################################################')

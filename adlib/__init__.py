921f365040a5a41757a7f5b9f7f391e08985bc93
Matthew Sedam <sedammatthew@gmail.com>
2018-06-27 15:24:06 -0500

    CVXPY: Force version 0.4.11

--- adlib/__init__.py
+++ adlib/__init__.py
@@ -1,10 +1,3 @@
-# Fix CVXPY as some adversaries use cvxpy==0.4.11
-
-import cvxpy
-
-if not hasattr(cvxpy, 'mul_elemwise'):
-    cvxpy.mul_elemwise = lambda c, x: c * x
-
 from . import adversaries
 from . import learners
 from . import tests

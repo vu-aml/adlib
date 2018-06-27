d9dbd2b14350d281cfe7f74b475f8a033baa8b84
Matthew Sedam <sedammatthew@gmail.com>
2018-06-27 14:21:36 -0500

    CVXPY fix

--- adlib/__init__.py
+++ adlib/__init__.py
@@ -1,6 +1,9 @@
 # Fix CVXPY as some adversaries use cvxpy==0.4.11
+
 import cvxpy
-cvxpy.mul_elemwise = lambda c, x: c * x
+
+if not hasattr(cvxpy, 'mul_elemwise'):
+    cvxpy.mul_elemwise = lambda c, x: c * x
 
 from . import adversaries
 from . import learners

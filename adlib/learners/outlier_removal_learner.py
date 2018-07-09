22845f845a21955d4f4a46df9e65330e3bf9be13
Matthew Sedam <sedammatthew@gmail.com>
2018-07-09 12:05:08 -0500

    ORL: Finish outlier removal learner

--- adlib/learners/outlier_removal_learner.py
+++ adlib/learners/outlier_removal_learner.py
@@ -5,6 +5,7 @@
 from adlib.learners.learner import Learner
 from adlib.utils.common import get_fvs_and_labels
 from typing import Dict
+import math
 import numpy as np
 
 
@@ -18,6 +19,7 @@ class OutlierRemovalLearner(Learner):
         self.training_instances = training_instances
         self.verbose = verbose
 
+        self.w = None
         self.mean = None
         self.std = None
 
@@ -30,14 +32,36 @@ class OutlierRemovalLearner(Learner):
             raise ValueError('Must have at least 2 instances to train.')
 
         fvs, labels = get_fvs_and_labels(self.training_instances)
+        fvs, labels = self._remove_outliers(fvs, labels)
+
+        self.w = np.full(fvs.shape[1], 0.0)
+        for i, fv in enumerate(fvs):
+            self.w += labels[i] * fv
+        self.w /= fvs.shape[0]
+
+    def _remove_outliers(self, fvs, labels):
+        """
+        Removes outliers
+        :param fvs: the feature vectors - np.ndarray
+        :param labels: the labels
+        :return: feature vectors and labels
+        """
+
+        cutoff = 10 * math.log(len(self.training_instances)) / fvs.shape[1]
+
+        if self.verbose:
+            print('Cutoff:', cutoff)
 
         self.mean = np.mean(fvs)
         self.std = np.std(fvs)
         fvs = (fvs - self.mean) / self.std
 
         iteration = 0
-        bad_instances = []
-        while iteration == 0 or len(bad_instances) > 0:
+        old_number_of_instances = -1
+        while old_number_of_instances != len(labels):
+            if self.verbose:
+                print('Iteration:', iteration, '- num_instances:', len(labels))
+
             matrix = np.full((fvs.shape[1], fvs.shape[1]), 0.0)
             for fv in fvs:
                 fv = fv.reshape((len(fv), 1))
@@ -45,22 +69,50 @@ class OutlierRemovalLearner(Learner):
 
             eigen_vals, eigen_vectors = np.linalg.eig(matrix)
 
-            for i, val in enumerate(eigen_vals):
-                eigen_vals[i] = np.linalg.norm(val)
+            tmp = []
+            for val in eigen_vals:
+                tmp.append(np.linalg.norm(val))
+            eigen_vals = tmp
 
+            # Find eigenvector with largest eigenvalue
             eigen_vals = list(enumerate(eigen_vals))
             eigen_vals.sort(key=lambda x: x[1])
             largest_index = eigen_vals[-1][0]
             v = eigen_vectors[largest_index]
 
+            new_fvs = []
+            new_labels = []
+            for i, fv in enumerate(fvs):
+                value = v.dot(fv) ** 2
+                if value < cutoff:
+                    new_fvs.append(fv)
+                    new_labels.append(labels[i])
+
+            old_number_of_instances = len(labels)
+            fvs = np.array(new_fvs)
+            labels = np.array(new_labels)
+
+            iteration += 1
+
+        return fvs, labels
+
     def predict(self, instances):
-        raise NotImplementedError
+        fvs, _ = get_fvs_and_labels(instances)
+        return list(map(lambda x: 1 if x > 0 else -1, self.decision_function(fvs)))
 
     def set_params(self, params: Dict):
-        raise NotImplementedError
+        if params['training_instances'] is not None:
+            self.training_instances = params['training_instances']
+        if params['verbose'] is not None:
+            self.verbose = params['verbose']
+
+        self.w = None
+        self.mean = None
+        self.std = None
 
     def predict_proba(self, X):
         raise NotImplementedError
 
     def decision_function(self, X):
-        raise NotImplementedError
+        X = (X - self.mean) / self.std
+        return X.dot(self.w)

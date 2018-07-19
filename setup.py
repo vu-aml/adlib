7a4cfec845b7068e2603efe0b559f3c0128654cc
Matthew Sedam <sedammatthew@gmail.com>
2018-07-19 10:17:22 -0500

    Update version to 1.0.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import find_packages, setup
 
 setup(
     name='adlib',
-    version='0.1.0',
+    version='1.0.0',
     description='Game-theoretic adversarial machine learning library providing '
                 'a set of learner and adversary modules.',
     url='https://github.com/vu-aml/adlib',

654aa162e78f982796cad8c2f534a938ecdc239e
Matthew Sedam <sedammatthew@gmail.com>
2018-07-26 11:43:29 -0500

    LF: Change max_iter default from 50 to 10

--- adlib/adversaries/label_flipping.py
+++ adlib/adversaries/label_flipping.py
@@ -19,7 +19,7 @@ class LabelFlipping(Adversary):
     """
 
     def __init__(self, learner, cost: List[float], total_cost: float,
-                 gamma=0.1, alpha=5e-7, max_iter=50, verbose=False):
+                 gamma=0.1, alpha=5e-7, max_iter=10, verbose=False):
         """
         :param learner: the previously-trained SVM learner
         :param cost: the cost vector, has length of size of instances
